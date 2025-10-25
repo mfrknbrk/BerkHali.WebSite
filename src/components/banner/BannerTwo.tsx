3558dff65100294f09ef9c33a15e7851b85b66e2
Muhammet Furkan Berk <mfrknbrkk@gmail.com>
2025-10-25 12:15:28 +0300

    Büyük Fotoğraf kısmı değiştirildi

--- src/components/banner/BannerTwo.tsx
+++ src/components/banner/BannerTwo.tsx
@@ -8,9 +8,9 @@ const BannerTwo = () => {
                 <Row>
                     <div className="banner-justify-box-contact col-12">
                         <div className="banner-two-box">
-                            <span>25% Off</span>
-                            <h4>Fresh & Organic vegetables</h4>
-                            <a onClick={(e) => e.preventDefault()} href="#" className="bb-btn-1">Shop Now</a>
+                            {/* <span>25% Off</span> */}
+                            <h4>Mekanınızı Baştan Tasarlayın</h4>
+                            <a onClick={(e) => e.preventDefault()} href="#" className="bb-btn-1">Keşfet</a>
                         </div>
                     </div>
                 </Row>
